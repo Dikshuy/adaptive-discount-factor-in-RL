345d8e764857104a3e17bdcef5838d178c9900df
Dikshuy <dikshant1212hsr@gmail.com>
2024-11-12 19:43:26 -0700

    weird shit happening with q_init

--- experiments/q_learning.py
+++ experiments/q_learning.py
@@ -74,24 +74,26 @@ def expected_return(env, Q, gamma, episodes = 10):
 def Q_learning(env, Q, gamma, eps, alpha, max_steps, _seed):
     exp_ret = []
     steps_per_episode = []
+    G = np.zeros(num_episodes)
     eps_decay = 4 * eps / max_steps
     alpha_decay = alpha / max_steps
     tot_steps = 0
     episodes = 0
 
-    while episodes < num_episodes:
+    while episodes < num_episodes:  
         s, _ = env.reset(seed = _seed, options = options)
         a = eps_greedy_action(Q, s, eps)
         done = False
         episode_steps = 0
 
-        while not done and tot_steps < max_steps:
+        while not done:# and tot_steps < max_steps:
             tot_steps += 1
             episode_steps += 1
             a = eps_greedy_action(Q, s, eps)
             s_next, r, terminated, truncated, _ = env.step(a)
 
             done = terminated or truncated
+            G[episodes] += gamma ** episode_steps * r
             eps = max(eps - eps_decay, 0.01)
             alpha = max(alpha - alpha_decay, 0.001)
 
@@ -102,9 +104,10 @@ def Q_learning(env, Q, gamma, eps, alpha, max_steps, _seed):
             Q[s,a] += alpha * td_err
 
             if done:
-                episodes += 1
-                exp_ret.append(expected_return(env, Q, gamma))
+                # exp_ret.append(expected_return(env, Q, gamma))
+                exp_ret.append(G[episodes])
                 steps_per_episode.append(episode_steps)
+                episodes += 1
 
             s = s_next
             a = a_next
@@ -139,9 +142,9 @@ def error_shade_plot(ax, data, stepsize, smoothing_window=1, **kwargs):
 alpha = 0.1
 eps = 1.0
 max_steps = 10000
-num_episodes = 150
+num_episodes = 500
 
-init_values = [0.0]
+init_values = [10.0]
 gamma_values = [0.1, 0.25, 0.5, 0.75, 0.8, 0.9, 0.99]
 seeds = np.arange(30)
 
@@ -184,7 +187,7 @@ for i, gamma in enumerate(gamma_values):
             results_exp_ret[i, j, seed] = exp_ret
             results_steps[i, j, seed] = steps
 
-            print(gamma, init_value, seed)
+            print(gamma, seed)
 
         error_shade_plot(
             axs[0],
@@ -196,7 +199,7 @@ for i, gamma in enumerate(gamma_values):
         axs[0].set_ylabel("Average Return", fontsize=10)
         axs[0].set_title("Q-Learning Performance Across Different Gamma Values")
         axs[0].legend()
-        axs[0].set_ylim([-5,1.4])
+        # axs[0].set_ylim([-5,1.4])
 
         error_shade_plot(
             axs[1],
@@ -208,7 +211,7 @@ for i, gamma in enumerate(gamma_values):
         axs[1].set_ylabel("Steps to Goal", fontsize=10)
         axs[1].set_title("Steps per Episode Across Different Gamma Values")
         axs[1].legend()
-        axs[1].set_ylim([0, 200]) 
+        # axs[1].set_ylim([0, 200]) 
 
         plt.tight_layout()
         plt.draw()
