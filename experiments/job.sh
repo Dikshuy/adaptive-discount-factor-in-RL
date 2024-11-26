e2ddd7191931a051a02832a997610b8b9843a008
Dikshuy <dikshant1212hsr@gmail.com>
2024-11-26 02:23:04 -0700

    code ready for CC

--- experiments/job.sh
+++ experiments/job.sh
@@ -2,19 +2,19 @@
 
 mkdir -p results/pendulum
 mkdir -p results/cartpole
+mkdir -p results/mountain_car
 mkdir -p results/acrobot
-mkdir -p results/mountain-car
 
 GAMMA_VALUES="0.1 0.5 0.8 0.99"
 ALPHA_ACTOR_VALUES="0.001"
 ALPHA_CRITIC_VALUES="0.01"
-EPISODES_EVAL=1
-N_SEEDS=3
+EPISODES_EVAL=10
+N_SEEDS=30
 
 echo "Running Pendulum Experiment..."
 
-EVAL_STEPS=5
-MAX_STEPS=100
+EVAL_STEPS=500
+MAX_STEPS=1000000
 EXPERIMENT_NAME="pendulum"
 
 python3 actor_critic_pendulum.py \
@@ -31,11 +31,12 @@ python3 actor_critic_pendulum.py \
 echo "Pendulum Experiment Completed"
 
 echo "-----------------------------------------------------------------------------------------"
+echo ""
 
 echo "Running CartPole Experiment..."
 
-EVAL_STEPS=5
-MAX_STEPS=200
+EVAL_STEPS=100
+MAX_STEPS=20000
 EXPERIMENT_NAME="cartpole"
 
 python actor_critic_cartpole.py \
@@ -52,32 +53,12 @@ python actor_critic_cartpole.py \
 echo "CartPole Experiment Completed"
 
 echo "-----------------------------------------------------------------------------------------"
-
-echo "Running Acrobot Experiment..."
-
-EVAL_STEPS=5
-MAX_STEPS=200
-EXPERIMENT_NAME="acrobot"
-
-python actor_critic_acrobot.py \
-    --gamma_values $GAMMA_VALUES \
-    --alpha_actor_values $ALPHA_ACTOR_VALUES \
-    --alpha_critic_values $ALPHA_CRITIC_VALUES \
-    --episodes_eval $EPISODES_EVAL \
-    --eval_steps $EVAL_STEPS \
-    --max_steps $MAX_STEPS \
-    --n_seeds $N_SEEDS \
-    --save_dir "results/acrobot" \
-    --experiment_name $EXPERIMENT_NAME
-
-echo "Acrobot Experiment Completed"
-
-echo "-----------------------------------------------------------------------------------------"
+echo ""
 
 echo "Running MoutainCar Experiment..."
 
-EVAL_STEPS=5
-MAX_STEPS=200
+EVAL_STEPS=500
+MAX_STEPS=1000000
 EXPERIMENT_NAME="mountain-car"
 
 python actor_critic_mountain_car.py \
@@ -92,6 +73,29 @@ python actor_critic_mountain_car.py \
     --experiment_name $EXPERIMENT_NAME
 
 echo "Mountain Car Experiment Completed"
+
+echo "-----------------------------------------------------------------------------------------"
+echo ""
+
+echo "Running Acrobot Experiment..."
+
+EVAL_STEPS=100
+MAX_STEPS=20000
+EXPERIMENT_NAME="acrobot"
+
+python actor_critic_acrobot.py \
+    --gamma_values $GAMMA_VALUES \
+    --alpha_actor_values $ALPHA_ACTOR_VALUES \
+    --alpha_critic_values $ALPHA_CRITIC_VALUES \
+    --episodes_eval $EPISODES_EVAL \
+    --eval_steps $EVAL_STEPS \
+    --max_steps $MAX_STEPS \
+    --n_seeds $N_SEEDS \
+    --save_dir "results/acrobot" \
+    --experiment_name $EXPERIMENT_NAME
+
+echo "Acrobot Experiment Completed"
+echo ""
 
 echo "========================================================================================="
 
