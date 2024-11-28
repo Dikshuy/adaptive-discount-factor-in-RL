b43d1955ab0ff7ba2f7e2bcf7607c7972a4b2b48
Dikshuy <dikshant1212hsr@gmail.com>
2024-11-27 21:36:09 -0700

    reduced seeds and number of max steps

--- experiments/job3.sh
+++ experiments/job3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-mtaylor3_cpu
 #SBATCH --mem-per-cpu=8G
-#SBATCH --time=60:00:00
+#SBATCH --time=36:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
@@ -11,12 +11,12 @@ GAMMA_VALUES="0.1 0.25 0.5 0.75 0.9 0.95 0.99"
 ALPHA_ACTOR_VALUES="0.001"
 ALPHA_CRITIC_VALUES="0.01"
 EPISODES_EVAL=10
-N_SEEDS=30
+N_SEEDS=15
 
 echo "Running MoutainCar Experiment..."
 
 EVAL_STEPS=500
-MAX_STEPS=1000000
+MAX_STEPS=500000
 EXPERIMENT_NAME="mountain-car"
 
 python3 actor_critic_mountain_car.py \
