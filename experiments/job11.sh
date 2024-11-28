d1d28c17427d9592d2811fe59a8a16064c1234fd
Dikshuy <dikshant1212hsr@gmail.com>
2024-11-27 22:45:20 -0700

    q learning  alpha testing

---
+++ experiments/job11.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p results/alpha/q_learning
+
+python q_learning.py \
+    --gamma_values 0.99 \
+    --alpha_values 0.05 0.1 0.5 \
+    --initial_values 0.0 10.0 \
+    --max_steps 200000 \
+    --eval_steps 100 \
+    --n_seeds 30 \
+    --save_dir "results/alpha/q_learning"
