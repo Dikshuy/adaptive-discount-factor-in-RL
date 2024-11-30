aba14ef5f44837e2a70c8886769c071175ef9950
Dikshuy <dikshant1212hsr@gmail.com>
2024-11-30 10:59:40 -0700

    auto job generator for CC

---
+++ experiments/create_jobs_MC.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INIT_VALUES="0 0.1 0.5 1"
+
+BASE_SCRIPT="job3.sh"
+
+for INIT in $INIT_VALUES; do
+    NEW_SCRIPT="MC_init_$INIT.sh"
+    
+    sed "s/INIT_VALUE=0/INIT_VALUE=$INIT/" $BASE_SCRIPT > $NEW_SCRIPT
+
+    chmod +x $NEW_SCRIPT
+    
+    echo "Generated script: $NEW_SCRIPT"
+done
+
+echo "All scripts created!"
+
+echo "--------------------------------------------------------------"
