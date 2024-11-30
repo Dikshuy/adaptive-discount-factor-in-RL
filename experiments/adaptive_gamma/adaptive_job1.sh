d37e3d565688b1455d701139044becf6f978447e
Dikshuy <dikshant1212hsr@gmail.com>
2024-11-29 18:32:06 -0700

    base codes for adaptive gamma

---
+++ experiments/adaptive_gamma/adaptive_job1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --account=def-mtaylor3_cpu
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=36:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+mkdir -p results/pendulum
+
+GAMMA_VALUES="0.1 0.25 0.5 0.75 0.9 0.95 0.99"
+ALPHA_ACTOR_VALUES="0.01"
+ALPHA_CRITIC_VALUES="0.01"
+EPISODES_EVAL=30
+N_SEEDS=25
+INIT_VALUE=0
+
+echo "Running Pendulum Experiment..."
+
+EVAL_STEPS=500
+MAX_STEPS=500000
+EXPERIMENT_NAME="pendulum"
+
+python3 actor_critic_pendulum.py \
+    --gamma_values $GAMMA_VALUES \
+    --init $INIT_VALUE \
+    --alpha_actor_values $ALPHA_ACTOR_VALUES \
+    --alpha_critic_values $ALPHA_CRITIC_VALUES \
+    --episodes_eval $EPISODES_EVAL \
+    --eval_steps $EVAL_STEPS \
+    --max_steps $MAX_STEPS \
+    --n_seeds $N_SEEDS \
+    --save_dir "results/pendulum" \
+    --experiment_name $EXPERIMENT_NAME
+
+echo "Pendulum Experiment Completed"
+
+echo "========================================================================================="
---
+++ experiments/adaptive_gamma/adaptive_job2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --account=def-mtaylor3_cpu
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=60:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+mkdir -p results/cartpole
+
+GAMMA_VALUES="0.1 0.25 0.5 0.75 0.9 0.95 0.99"
+ALPHA_ACTOR_VALUES="0.01"
+ALPHA_CRITIC_VALUES="0.01"
+EPISODES_EVAL=25
+N_SEEDS=30
+INIT_VALUE=0
+
+echo "Running CartPole Experiment..."
+
+EVAL_STEPS=500
+MAX_STEPS=500000
+EXPERIMENT_NAME="cartpole"
+
+python3 actor_critic_cartpole.py \
+    --gamma_values $GAMMA_VALUES \
+    --init $INIT_VALUE \
+    --alpha_actor_values $ALPHA_ACTOR_VALUES \
+    --alpha_critic_values $ALPHA_CRITIC_VALUES \
+    --episodes_eval $EPISODES_EVAL \
+    --eval_steps $EVAL_STEPS \
+    --max_steps $MAX_STEPS \
+    --n_seeds $N_SEEDS \
+    --save_dir "results/cartpole" \
+    --experiment_name $EXPERIMENT_NAME
+
+echo "CartPole Experiment Completed"
+
+echo "========================================================================================="
