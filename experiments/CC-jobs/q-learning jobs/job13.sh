cb8a1d72e3f69a5a48fcc7ceb5b685f563cb715f
Dikshuy <dikshant1212hsr@gmail.com>
2024-12-03 01:08:57 -0700

    jobs for q learning

--- experiments/CC-jobs/q-learning jobs/job13.sh
+++ experiments/CC-jobs/q-learning jobs/job13.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
-mkdir -p results/q_learning
+mkdir -p results/q_learning/adaptive_gamma
 
 python q_learning.py \
     --environments EASY_SPARSE EASY_MEDIUM EASY_DENSE MODERATE_SPARSE MODERATE_MEDIUM MODERATE_DENSE DIFFICULT_SPARSE DIFFICULT_MEDIUM DIFFICULT_DENSE \
@@ -13,7 +13,17 @@ python q_learning.py \
     --gamma_values 0.1 0.25 0.5 0.75 0.9 0.99 \
     --alpha_values 0.1 \
     --initial_values 0.0 \
-    --max_steps 250000 \
+    --max_steps 400000 \
     --eval_steps 100 \
     --n_seeds 50 \
-    --save_dir "results/q_learning"
+    --save_dir "results/q_learning/adaptive_gamma"
+
+python q_learning.py \
+    --environments EASY_SPARSE EASY_MEDIUM EASY_DENSE MODERATE_SPARSE MODERATE_MEDIUM MODERATE_DENSE DIFFICULT_SPARSE DIFFICULT_MEDIUM DIFFICULT_DENSE \
+    --gamma_values 0.1 0.25 0.5 0.75 0.9 0.99 \
+    --alpha_values 0.1 \
+    --initial_values 0.0 \
+    --max_steps 400000 \
+    --eval_steps 100 \
+    --n_seeds 50 \
+    --save_dir "results/q_learning/"
