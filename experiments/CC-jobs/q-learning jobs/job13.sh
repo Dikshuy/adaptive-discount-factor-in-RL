7b8045786030119f37bad309dffc1bc712831fff
Dikshuy <dikshant1212hsr@gmail.com>
2024-12-03 01:36:19 -0700

    updates

--- experiments/CC-jobs/q-learning jobs/job13.sh
+++ experiments/CC-jobs/q-learning jobs/job13.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-mtaylor3_cpu
 #SBATCH --mem-per-cpu=2G
-#SBATCH --time=5:00:00
+#SBATCH --time=7:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
@@ -17,7 +17,7 @@ python q_learning.py \
     --max_steps 400000 \
     --eval_steps 100 \
     --n_seeds 50 \
-    --save_dir "../../results/q_learning/adaptive_gamma"
+    --save_dir "../../results/q_learning/adaptive"
 
 python q_learning.py \
     --environments EASY_SPARSE EASY_MEDIUM EASY_DENSE MODERATE_SPARSE MODERATE_MEDIUM MODERATE_DENSE DIFFICULT_SPARSE DIFFICULT_MEDIUM DIFFICULT_DENSE \
