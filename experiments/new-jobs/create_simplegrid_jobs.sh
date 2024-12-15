a8080c337ce952a0a473a6a006f721e525009880
Dikshuy <dikshant1212hsr@gmail.com>
2024-12-15 16:22:00 -0700

    temp for CC

--- experiments/new-jobs/create_simplegrid_jobs.sh
+++ experiments/new-jobs/create_simplegrid_jobs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-Q_INIT_VALUES="0 1 5 10"
-ALPHA_VALUES="0.1 0.5 1"
+Q_INIT_VALUES="0 5 10"
+ALPHA_VALUES="0.05 0.1 0.25 0.5"
+STOCHASTICITY="0.0 0.01 0.1"
 
 BASE_SCRIPT="job_simplegrid.sh"
 
@@ -10,22 +11,24 @@ mkdir -p $JOB_DIR
 
 for INIT in $Q_INIT_VALUES; do
     for ALPHA in $ALPHA_VALUES; do
-        JOB_FILE="$JOB_DIR/job_init_${INIT}_alpha_${ALPHA}.sh"
-        
-        sed -e "s/Q_INIT=0/Q_INIT=$INIT/" \
-            -e "s/ALPHA=0.1/ALPHA=$ALPHA/" \
-            $BASE_SCRIPT > $JOB_FILE
+        for STOCHS in $STOCHASTICITY; do
+            JOB_FILE="$JOB_DIR/job_init_${INIT}_alpha_${ALPHA}_stoch_${STOCHS}.sh"
+            
+            sed -e "s/Q_INIT=0/Q_INIT=$INIT/" \
+                -e "s/ALPHA=0.1/ALPHA=$ALPHA/" \
+                -e "s/STOCHS=0.1/STOCHS=$STOCHS/" \
+                $BASE_SCRIPT > $JOB_FILE
 
-        chmod +x ${JOB_FILE}
+            chmod +x ${JOB_FILE}
 
-        echo "Generate job file: $JOB_FILE"
+            echo "Generate job file: $JOB_FILE"
 
-        # sbatch $JOB_FILE
-        bash $JOB_FILE
+            # sbatch $JOB_FILE
+            bash $JOB_FILE
 
-        echo "submitted job: $JOB_FILE"
+            echo "submitted job: $JOB_FILE"
 
-        echo "================================="
+            echo "================================="
   done
 done
 
